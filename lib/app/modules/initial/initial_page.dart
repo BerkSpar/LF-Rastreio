bc50041e34c59250514a5c42a84fe134db137531
Felipe Passos <berkspar@gmail.com>
2021-02-24 20:18:24 -0300

    update colors

--- lib/app/modules/initial/initial_page.dart
+++ lib/app/modules/initial/initial_page.dart
@@ -1,3 +1,4 @@
+import 'package:fl_rastreio/app/shared/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -18,7 +19,7 @@ class _InitialPageState extends ModularState<InitialPage, InitialController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: LFColors.black,
       body: Center(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16),
@@ -28,7 +29,7 @@ class _InitialPageState extends ModularState<InitialPage, InitialController> {
               Image.asset('assets/images/lf-logo.png'),
               SizedBox(height: 16),
               SpinKitWave(
-                color: Colors.white,
+                color: LFColors.lightBlue,
                 size: 24.0,
               ),
             ],
