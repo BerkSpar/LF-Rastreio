8859fc43d36c32c03edf3ad6b011a20aa8e6fb45
Felipe Passos <berkspar@gmail.com>
2021-02-24 14:04:47 -0300

    add splash screen

--- lib/app/modules/initial/initial_page.dart
+++ lib/app/modules/initial/initial_page.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'initial_controller.dart';
 
 class InitialPage extends StatefulWidget {
-  final String title;
-  const InitialPage({Key key, this.title = "Initial"}) : super(key: key);
-
   @override
   _InitialPageState createState() => _InitialPageState();
 }
 
 class _InitialPageState extends ModularState<InitialPage, InitialController> {
-  //use 'controller' variable to access controller
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/lf-logo.png'),
+              SizedBox(height: 16),
+              SpinKitWave(
+                color: Colors.white,
+                size: 24.0,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
