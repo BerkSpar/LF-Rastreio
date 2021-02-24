6ad39ab4c9c3be4f3095c059e0322fd8bf3979bd
Felipe Passos <berkspar@gmail.com>
2021-02-24 20:18:09 -0300

    add LFColors

---
+++ lib/app/shared/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LFColors {
+  static const Color black = Color(0xFF1E1F26);
+  static const Color darkBlue = Color(0xFF283655);
+  static const Color mediumBlue = Color(0xFF4d648d);
+  static const Color lightBlue = Color(0xFFd0e1f9);
+  static const Color white = Color(0xFFFFFFFF);
+}
