bb8e73fcd25b291012608a0e3941ed144a0bfb6a
Felipe Passos <berkspar@gmail.com>
2021-02-25 02:34:25 -0300

    add pull to refresh

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -2,7 +2,7 @@ import 'package:fl_rastreio/app/shared/models/vehicle.dart';
 import 'package:fl_rastreio/app/shared/repositories/lf_rastreio_api_repository.dart';
 import 'package:mobx/mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:asuka/asuka.dart' as asuka;
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 part 'home_controller.g.dart';
 
@@ -11,11 +11,14 @@ class HomeController = _HomeControllerBase with _$HomeController;
 
 abstract class _HomeControllerBase with Store {
   final api = Modular.get<LfRastreioApiRepository>();
+  final refreshController = RefreshController();
 
   @observable
   List<Vehicle> vehicles = [];
 
   onRefresh() async {
     vehicles = await api.getVehicles();
+
+    refreshController.refreshCompleted();
   }
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:fl_rastreio/app/widgets/vehicle_card/vehicle_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -26,30 +27,35 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
       ),
       body: Observer(
         builder: (context) {
-          return ListView.separated(
-            padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
-            separatorBuilder: (_, index) {
-              return SizedBox(height: 8);
-            },
-            itemCount: controller.vehicles.length,
-            itemBuilder: (_, index) {
-              final vehicle = controller.vehicles[index];
+          return SmartRefresher(
+            controller: controller.refreshController,
+            enablePullDown: true,
+            onRefresh: controller.onRefresh,
+            child: ListView.separated(
+              padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
+              separatorBuilder: (_, index) {
+                return SizedBox(height: 8);
+              },
+              itemCount: controller.vehicles.length,
+              itemBuilder: (_, index) {
+                final vehicle = controller.vehicles[index];
 
-              return VehicleCardWidget(
-                vehicle: vehicle,
-                onTap: () => showModalBottomSheet(
-                  context: context,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(32),
+                return VehicleCardWidget(
+                  vehicle: vehicle,
+                  onTap: () => showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    builder: (_) {
+                      return VehicleBottomWidget(
+                        vehicle: vehicle,
+                      );
+                    },
                   ),
-                  builder: (_) {
-                    return VehicleBottomWidget(
-                      vehicle: vehicle,
-                    );
-                  },
-                ),
-              );
-            },
+                );
+              },
+            ),
           );
         },
       ),
